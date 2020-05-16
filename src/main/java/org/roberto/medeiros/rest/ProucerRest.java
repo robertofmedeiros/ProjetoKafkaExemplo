f124c50122905450fa573e2b95d974acc7b485fe
robertofmedeiros <roberto_p.v@outlook.com>
2020-05-16 12:18:45 -0300

    - Teste deplou webhook github

--- src/main/java/org/roberto/medeiros/rest/ProucerRest.java
+++ src/main/java/org/roberto/medeiros/rest/ProucerRest.java
@@ -19,6 +19,7 @@ public class ProucerRest {
 
     @PostMapping(value = "/publish")
     public void sendMessageToKafkaTopic(@RequestParam("message") String message){
+
         this.producer.sendMessage(message);
     }
 
